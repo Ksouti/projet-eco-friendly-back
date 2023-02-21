d7764a15685e2c296d9a978e86bf91f258da683f
Abbad <devabbad78@gmail.com>
2023-02-21 11:37:39 +0100

    Feat: UserType

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,9 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +16,13 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles',ChoiceType::class,[
+            ->add('email',EmailType::class,[
+                "label" => "L'email",
+                "attr" => [
+                    "placeholder" => "L'email"
+                ]
+            ])
+                ->add('roles',ChoiceType::class,[
                 "choices"=>[
                     "Manager" => "ROLE_AUTHOR",
                     "Admin" => "ROLE_ADMIN"
@@ -22,14 +30,44 @@ class UserType extends AbstractType
                 "expanded" => true,
                 "multiple" => true
             ])
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('nickname')
-            ->add('avatar')
-            ->add('is_active')
-            ->add('created_at')
-            ->add('updated_at')
+           
+            ->add("firstname", TextType::class, [
+                "label" => "Prénom",
+                "attr" => [
+                    "placeholder" => "Entrez votre prénom"
+                ]
+            ])
+
+            ->add("lastname", TextType::class, [
+                "label" => "Nom de famille",
+                "attr" => [
+                    "placeholder" => "Entrez votre nom de famille"
+                ]
+            ])
+
+            ->add("nickname", TextType::class, [
+                "label" => 'Pseudo',
+                "attr" => [
+                    "placeholder" => "Entrez votre pseudo"
+                ]
+            ])
+
+            ->add('avatar',UrlType::class,[
+                "label" => "Votre avatar *",
+                "attr" => [
+                    "placeholder" => "Votre avatar"
+                ],
+                "help"=> "* L'url d'un avatar"
+            ]) 
+
+            ->add('is_active' ,ChoiceType::class,[
+                "choices" => [
+                    "Activé" => "0",
+                    "dasactivé" => "1"
+                ],
+                "label" => "Activé ou désactivé"
+            ])
+           
         ;
     }
 
