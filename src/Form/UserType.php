42e5573f5f58dacb5d0287aa91e3cae5e5256416
Abbad <devabbad78@gmail.com>
2023-02-21 11:37:39 +0100

    Feat: UserType

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -16,60 +16,24 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class, [
-                "label" => "L'email",
-                "attr" => [
-                    "placeholder" => "L'email"
-                ]
-            ])
-                ->add('roles', ChoiceType::class, [
-                "choices" => [
-                    "Auteur" => "ROLE_AUTHOR",
-                    "Admin" =>  "ROLE_ADMIN",
-                    "User" =>   "ROLE_USER"
+            ->add('email')
+            ->add('roles',ChoiceType::class,[
+                "choices"=>[
+                    "Manager" => "ROLE_AUTHOR",
+                    "Admin" => "ROLE_ADMIN"
                 ],
                 "expanded" => true,
                 "multiple" => true
             ])
-           
-            ->add("firstname", TextType::class, [
-                "label" => "Prénom",
-                "attr" => [
-                    "placeholder" => "Entrez votre prénom"
-                ]
-            ])
-
-            ->add("lastname", TextType::class, [
-                "label" => "Nom de famille",
-                "attr" => [
-                    "placeholder" => "Entrez votre nom de famille"
-                ]
-            ])
-
-            ->add("nickname", TextType::class, [
-                "label" => 'Pseudo',
-                "attr" => [
-                    "placeholder" => "Entrez votre pseudo"
-                ]
-            ])
-
-            ->add('avatar', UrlType::class, [
-                "label" => "Votre avatar *",
-                "attr" => [
-                    "placeholder" => "Votre avatar"
-                ],
-                "help" => "* L'url d'un avatar"
-            ]) 
-/*
-            ->add('is_active' ,ChoiceType::class,[
-                "choices" => [
-                    "Activé" => "0",
-                    "Désactivé" => "1"
-                ],
-                "label" => "Activé ou désactivé"
-            ])
-     */;
-
+            ->add('password')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('nickname')
+            ->add('avatar')
+            ->add('is_active')
+            ->add('created_at')
+            ->add('updated_at')
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
