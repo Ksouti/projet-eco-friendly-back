d4ccf9a0c67de0e9720daa09d689f996f053c93e
Abbad <devabbad78@gmail.com>
2023-02-21 11:10:41 +0100

    FEAT: CategoryType

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +13,20 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('tagline')
-            ->add('slug')
+            ->add('name',TextType::class,[
+                "label" => "Nom de la catégorie",
+                "attr" => [
+                    "placeholder" => "Nom de la catégorie"
+                ]
+            ])
+
+            ->add('tagline',TextType::class,[
+                "label" => "Phrase d'accroche",
+                "attr" => [
+                    "placeholder" => "Phrase d'accroche"
+                ]
+            ])
+            
         ;
     }
 
