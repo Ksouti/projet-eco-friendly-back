e52a0284736e8f58f05e10e90e5a44fb85626f73
Laure-Riglet <laure.riglet@gmail.com>
2023-02-27 23:14:24 +0100

    BUILD: Webpack Encore setup

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,17 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './styles/bootstrap.min.css';
+import './styles/styles.css';
 
 // start the Stimulus application
 import './bootstrap';
+import './js/bootstrap.bundle.min';
+import './js/popper.min';
+
+
+// Bootstrap tooltips 
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
--- public/assets/js/app.js
+++
@@ -1,6 +0,0 @@
-// Bootstrap tooltips 
-
-const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl);
-});
