d693045da1f7bf874a160f7ae15f122e3d822737
Laure-Riglet <laure.riglet@gmail.com>
2023-03-01 12:16:39 +0100

    FIX: webpack encore removed

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
