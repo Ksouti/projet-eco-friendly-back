73c7a5e79037b55d35f08b5b097a71cd4ddace5d
Laure-Riglet <laure.riglet@gmail.com>
2023-03-01 22:24:08 +0100

    FEAT: Avatar upload via POST

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Repository\AdviceRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -81,32 +82,38 @@ class UserController extends AbstractController
     /**
      * @Route("/api/users/{id}/avatar", name="app_api_users_avatar", requirements={"id":"\d+"}, methods={"POST"})
      */
-    public function avatarUpload(Request $request, ?User $user, Serializer $serializer): Response
+    public function avatarUpload(Request $request, ?User $user, UserRepository $userRepository): Response
     {
         if (!$user) {
             return $this->json(['errors' => ['Utilisateur' => 'Cet utilisateur n\'existe pas']], Response::HTTP_NOT_FOUND);
         }
 
+        $avatar = $request->files->get('avatar');
+
+        if (!$avatar) {
+            return $this->json(['errors' => 'Image non valide'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $filename = $user->getId() . '-' . uniqid() . '.' . $avatar->guessExtension();
+
         try {
-            $user->$serializer->deserialize($request->getContent(), User::class, 'json');
-            $user->setAvatar($data['avatar'] ?? $user->getAvatar());
-            $user->setUpdatedAt(new \DateTimeImmutable());
-        } catch (NotEncodableValueException $e) {
-            return $this->json(['errors' => 'Json non valide'], Response::HTTP_BAD_REQUEST);
-        }
-
-        $errors = $validator->validate($user);
-
-        if (count($errors) > 0) {
-            $errorsArray = [];
-            foreach ($errors as $error) {
-                $errorsArray[$error->getPropertyPath()][] = $error->getMessage();
-            }
-            return $this->json(
-                ['errors' => $errorsArray],
-                Response::HTTP_UNPROCESSABLE_ENTITY
+            $avatar->move(
+                $this->getParameter('uploads_user_directory'),
+                $filename
             );
+            $user->setAvatar($this->getParameter('uploads_user_url') . $filename);
+        } catch (FileException $e) {
+            return $this->json(['errors' => 'Une erreur est survenue lors de l\'upload de l\'image'], Response::HTTP_BAD_REQUEST);
         }
+
+        $userRepository->add($user, true);
+
+        return $this->json(
+            $user,
+            Response::HTTP_OK,
+            ['Location' => $this->generateUrl('app_api_users_read', ['id' => $user->getId()])],
+            ['groups' => 'users']
+        );
     }
 
     /**
