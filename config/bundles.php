7301ef275013b3bbb48adc93cc143f60d941d1d5
Laure-Riglet <laure.riglet@gmail.com>
2023-03-02 09:50:26 +0100

    WIP: Api register

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
--- src/Controller/Api/RegistrationController.php
+++ src/Controller/Api/RegistrationController.php
@@ -24,12 +24,12 @@ use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class RegistrationController extends AbstractController
 {
-    //    private EmailVerifier $emailVerifier;
-    //
-    //    public function __construct(EmailVerifier $emailVerifier)
-    //    {
-    //        $this->emailVerifier = $emailVerifier;
-    //    }
+    private EmailVerifier $emailVerifier;
+
+    public function __construct(EmailVerifier $emailVerifier)
+    {
+        $this->emailVerifier = $emailVerifier;
+    }
 
     /**
      * @Route("/api/register", name="app_api_users_register", methods={"POST"})
@@ -39,7 +39,10 @@ class RegistrationController extends AbstractController
         try {
             $user = $serializer->deserialize($request->getContent(), User::class, 'json');
             $user->setRoles(['ROLE_USER']);
+            $user->setIsActive(true);
+            $user->setIsVerified(false);
             $user->setCreatedAt(new DateTimeImmutable());
+            $user->setPassword($userPasswordHasher->hashPassword($user, $user->getPassword()));
         } catch (NotEncodableValueException $e) {
             return $this->json(['errors' => 'Json non valide'], Response::HTTP_BAD_REQUEST);
         }
@@ -54,67 +57,37 @@ class RegistrationController extends AbstractController
             return $this->json(['errors' => $errorsArray], Response::HTTP_BAD_REQUEST);
         }
 
-        $user->setPassword($userPasswordHasher->hashPassword($user, $user->getPassword()));
+        $userRepository->add($user, true);
 
-        $email = (new TemplatedEmail())
-            ->from(new Address('no-reply@eco-friendly.fr'))
-            ->to($user->getEmail())
-            ->subject('Confirmez votre adresse email et rejoignez-nous !')
-            ->htmlTemplate('email/email_verification.html.twig')
-            ->context([
-                'user' => $user,
-                'url' => 'http://localhost:8000/',
-            ]);
+        // generate a signed url and email it to the user
+        $this->emailVerifier->sendEmailConfirmation(
+            'app_verify_email',
+            $user,
+            (new TemplatedEmail())
+                ->from(new Address('no-reply@eco-friendly.fr', 'Eco-Friendly'))
+                ->to($user->getEmail())
+                ->subject('Confirmez votre adresse email et rejoignez-nous !')
+                ->htmlTemplate('email/email_verification.html.twig')
+        );
+        // do anything else you need here, like send an email
+        return $this->json($userRepository->find($user->getId()), Response::HTTP_OK, [], ['groups' => 'users']);
+    }
 
+
+    /**
+     * @Route("/verify/email", name="app_verify_email")
+     */
+    public function verifyUserEmail(Request $request, TranslatorInterface $translator, UserRepository $userRepository): Response
+    {
+        // $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // validate email confirmation link, sets User::isVerified=true and persists
         try {
-            $mailer->send($email);
-        } catch (\Exception $e) {
-            return $this->json(['errors' => $e], Response::HTTP_INTERNAL_SERVER_ERROR);
+            $this->emailVerifier->handleEmailConfirmation($request, $request->getUser());
+        } catch (VerifyEmailExceptionInterface $exception) {
+            return $this->json(['errors' => $translator->trans($exception->getReason())], Response::HTTP_BAD_REQUEST);
         }
 
-        return $this->json('moui?', Response::HTTP_OK);
-
-        //
-        //        // generate a signed url and email it to the user
-        //        $this->emailVerifier->sendEmailConfirmation(
-        //            'app_verify_email',
-        //            $user,
-        //            (new TemplatedEmail())
-        //                ->from(new Address('laure.riglet@gmail.com', 'L4ur3l3i\'s MailBot'))
-        //                ->to($user->getEmail())
-        //                ->subject('Confirmez votre adresse email et rejoignez-nous !')
-        //                ->htmlTemplate('registration/confirmation_email.html.twig')
-        //        );
-        //        // do anything else you need here, like send an email
-        //
-        //        return $this->redirectToRoute('app_admin');
-        //    }
-        //
-        //    #         return $this->render('registration/register.html.twig', [
-        //    #             'registrationForm' => $form->createView(),
-        //    #         ]);
-        //    #     }
-        //
-        /**
-         * @Route("/verify/email", name="app_verify_email")
-         */
-        //    public function verifyUserEmail(Request $request, TranslatorInterface $translator): Response
-        //    {
-        //        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-        //
-        //        // validate email confirmation link, sets User::isVerified=true and persists
-        //        try {
-        //            $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
-        //        } catch (VerifyEmailExceptionInterface $exception) {
-        //            $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
-        //
-        //            return $this->redirectToRoute('app_register');
-        //        }
-        //
-        //        // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        //        $this->addFlash('success', 'Your email address has been verified.');
-        //
-        //        return $this->redirectToRoute('app_register');
-        //    }
+        return $this->json($userRepository->find($request->getUser()), Response::HTTP_OK, [], ['groups' => 'users']);
     }
 }
